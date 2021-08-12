aea3481e2725705263659daf5b14d7e153bd0563
ryousora <761745996@qq.com>
2021-08-12 17:47:02 +0800

    change host

--- src/pages/LoadPage.tsx
+++ src/pages/LoadPage.tsx
@@ -25,7 +25,7 @@ export const LoadPage: FC<LoadPageProps> = function ({onLoad}) {
     const {updateParsedData, updatePage} = useGlobalContext();
     const params = qs.parse(document.location.search, {ignoreQueryPrefix: true})
     const uid = params.uid
-    const host = "http://yba.asia:7701"
+    const host = "http://gachalog.lxx.name"
     // const host = "http://127.0.0.1:7701"
     if (uid) {
         fetch(`${host}/genshin/gachalog/xlsx/${uid}`)
