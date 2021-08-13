b330fe04ca0d04647f25b21cfce61f5a7e8880f1
ryousora <761745996@qq.com>
2021-08-13 13:38:34 +0800

    https

--- src/pages/LoadPage.tsx
+++ src/pages/LoadPage.tsx
@@ -25,7 +25,7 @@ export const LoadPage: FC<LoadPageProps> = function ({onLoad}) {
     const {updateParsedData, updatePage} = useGlobalContext();
     const params = qs.parse(document.location.search, {ignoreQueryPrefix: true})
     const uid = params.uid
-    const host = "http://genshin.lxx.name"
+    const host = "https://genshin.lxx.name"
     // const host = "http://127.0.0.1:7701"
     if (uid) {
         fetch(`${host}/genshin/gachalog/xlsx/${uid}`)
